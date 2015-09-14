db0106f6ccd726f372d84df30a0fa70dd77d5d6f
Del Stevens <del.stevens@gmail.com>
2015-09-14 13:46:17 +1200

    ex #1

--- process_arg.js
+++ process_arg.js
@@ -1,6 +1,6 @@
 var inArray = process.argv
 var outVal = 0
 for (var i = 2; i < inArray.length; i++){
-  outVal = outVal + Number(inArray[i])  
+  outVal += Number(inArray[i])  
 }
 console.log(outVal)
