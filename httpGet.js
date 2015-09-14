3c3f2458aa81bcecc6890cdf45eda9072902becc
Del Stevens <del.stevens@gmail.com>
2015-09-15 10:42:33 +1200

    http get example

--- httpGet.js
+++ httpGet.js
@@ -1,7 +1,13 @@
 var http = require ('http')
 
-http.get("http://google.co.uk", function(returnData) {
-  console.log("Returned status code is: " + returnData.statusCode)
-}).on('error', function(e) {
-  console.log("An error was returned: " + e.message)
+var url = process.argv[2]
+
+http.get(url, function(returnData) {
+  //console.log(returnData.statusCode)
+  returnData.setEncoding('utf8')
+  returnData.on('data', function (chunk) {
+    console.log(chunk);
+  });
+}).on('error', function(err) {
+  console.log("An error was returned: " + err.message)
 });
