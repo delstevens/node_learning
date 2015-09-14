b660c713ec1bff0cd63b02f476a9ae9d9b05830c
Del Stevens <del.stevens@gmail.com>
2015-09-14 13:43:41 +1200

    learnyounode exercise #2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
+  response.write("Changed Port Number");
   response.end();
-}).listen(8888);
+}).listen(3000);
